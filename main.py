8da50a8ab6ad6717efa5508bc441c2d799883726
durkisneer1 <martinez.d048432@stu.sanjac.edu>
2024-10-01 13:09:42 -0500

    upload starter files

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+from constants import *
+
+pygame.init()
+screen = pygame.display.set_mode(WIN_SIZE, pygame.SCALED)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    pygame.display.flip()
